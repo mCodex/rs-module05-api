fe0d49152e9e344e8c6cead01a88afc9d1bfc957
Mateus Andrade <mat.andrade@live.com>
2020-05-25 16:29:48 -0300

    implementing few repositories

--- src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
+++ src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
@@ -12,11 +12,22 @@ class OrdersRepository implements IOrdersRepository {
   }
 
   public async create({ customer, products }: ICreateOrderDTO): Promise<Order> {
-    // TODO
+    console.log(customer)
+    console.log(products)
+    const order = this.ormRepository.create({
+      customer: customer,
+      order_products: products
+    })
+
+    await this.ormRepository.save(order)
+
+    return order
   }
 
   public async findById(id: string): Promise<Order | undefined> {
-    // TODO
+    const order = this.ormRepository.findOne(id)
+
+    return order
   }
 }
 
